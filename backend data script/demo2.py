77e8bcf4778af67c59195a47e0d82fe6c49c736d
Darshak Vasoya <69636942+DarshakVasoya@users.noreply.github.com>
2024-01-01 14:20:02 +0100

    Delete backend data script/demo2.py

--- backend data script/demo2.py
+++
@@ -1,9 +0,0 @@
-import bs4
-import json
-import requests
-import html5lib
-from  firebase import firebase
-
-firebase = firebase.FirebaseApplication('https://technical-friend-5a7b7.firebaseio.com', None)
-
-result = firebase.put('', 'News', 'a')
